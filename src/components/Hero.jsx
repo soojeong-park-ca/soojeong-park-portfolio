8c8079157c2ac09a312b6e9b1e76ed53ecb5bc5e
Soojeong Park <soojeong.park.ca@gmail.com>
2023-05-19 13:23:24 -0700

    move typography related styles to typography scss file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Hero() {
   return (
     <section id="section__hero" className="container section__hero">
@@ -6,6 +8,9 @@ export default function Hero() {
           Hi, my name is <span>Soojeong Park</span> and I'm a{" "}
           <span>frontend web developer</span>.
         </h1>
+        <Link to="contact" className="btn--to-contact">
+          Get in touch
+        </Link>
       </div>
       <div className="hero-bg"></div>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import SectionAbout from "../components/SectionAbout";
 
 export default function Home() {
   return (
     <main className="main">
       <Hero />
+      <SectionAbout />
     </main>
   );
 }
