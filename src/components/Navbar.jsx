4b18c71ed1e325e73039d96e2b81ed82c5c80dfd
Soojeong Park <soojeong.park.ca@gmail.com>
2023-05-19 10:04:23 -0700

    add box shadow to navbar on scroll, change nav overlay to be position from top: 0

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar({ navOpen, onNavBtnClick }) {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = function () {
+      if (window.scrollY > 50) setScrolled(true);
+      else {
+        setScrolled(false);
+      }
+    };
+  }, [window.scrollY]);
+
   return (
-    <div className="navbar">
+    <div className={`navbar ${scrolled && "navbar-box-shadow"}`}>
       <div className="container nav-container">
         <Link to="/" className="logo">
           <h4 className="logo__text">Soojeong P</h4>
