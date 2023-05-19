219068291d8ca55e9f05932d89c15362f0f1ed16
Soojeong Park <soojeong.park.ca@gmail.com>
2023-05-18 17:08:17 -0700

    add footer, footer has a bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,33 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-  return <footer>Footer here</footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__title">
+          <h4>
+            Looking for a frontend web developer?
+            <br />
+            Let's work together.
+          </h4>
+          <Link to="/contact" className="btn--to-contact">
+            Get in touch
+          </Link>
+        </div>
+        <div className="footer__contact">
+          <div className="sns-buttons">
+            <a href="" className="btn--sns">
+              GitHub
+            </a>
+            <a href="" className="btn--sns">
+              LinkedIn
+            </a>
+          </div>
+          <div className="copyright">
+            <p>&copy; 2023 - Soojeong Park</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
