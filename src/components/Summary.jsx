4313c29d2b7971fab27179a29e7d32de6cb94c25
Soojeong Park <soojeong.park.ca@gmail.com>
2023-05-23 18:39:50 -0700

    add skills on home page

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import ResponsiveIcon from "./ResponsiveIcon";
 import ProjectItem from "./ProjectItem";
 import myImg from "../assets/images/pfp.png";
 
@@ -14,9 +15,9 @@ export default function Summary() {
   ));
 
   return (
-    <section id="section__summary" className="container section__summary">
+    <section id="section__summary" className="section__summary">
       <div className="summary">
-        <div className="summary__about">
+        <div className="container summary__about">
           <h2 className="summary__title">A little bit about me</h2>
           <div className="summary__body">
             <div className="summary__text">
@@ -42,7 +43,104 @@ export default function Summary() {
           </div>
         </div>
 
-        <div className="summary__projects">
+        <div className="summary__skills">
+          <div className="container skills__container reel">
+            <div className="skill">
+              <figure>
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"
+                  alt="HTML5 Logo"
+                />
+                <figcaption>HTML5</figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure>
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"
+                  alt="CSS3 Logo"
+                />
+                <figcaption>CSS3</figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure>
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+                  alt="JavaScript Logo"
+                />
+                <figcaption>JavaScript</figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure>
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+                  alt="React Logo"
+                />
+                <figcaption>React</figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure>
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg"
+                  alt="Sass Logo"
+                />
+                <figcaption>Sass</figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure>
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg"
+                  alt="Git Logo"
+                />
+                <figcaption>Git</figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure>
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg"
+                  alt="TailwindCSS Logo"
+                />
+                <figcaption>TailwindCSS</figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure className="skill-learning">
+                <img
+                  className="skill-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
+                  alt="Redux Logo"
+                />
+                <figcaption>
+                  Redux
+                  <br />
+                  (learning)
+                </figcaption>
+              </figure>
+            </div>
+            <div className="skill">
+              <figure>
+                <ResponsiveIcon />
+                <figcaption style={{ textAlign: "center" }}>
+                  Responsive
+                </figcaption>
+              </figure>
+            </div>
+          </div>
+        </div>
+
+        <div className="container summary__projects">
           <h2 className="summary__title">
             Take a peek at what I've been building
           </h2>
